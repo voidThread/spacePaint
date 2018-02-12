217757095f8ec50cbde24668a5156a7fe965ef21
voidThread <dirtysqueeze@gmail.com>
2018-02-13 00:06:31 +0100

    add cpp file for global variables - omit multiple declaration when including header in multiple translation units

---
+++ GlobalNames.cpp
@@ -0,0 +1,9 @@
+//
+// Created by dawid on 13.02.18.
+//
+
+#include <GlobalNames.h>
+
+QString GLOBAL_STRINGS::PROGRAM_INFO::PROGRAM_NAME(QObject::tr("spacePaint"));
+QString GLOBAL_STRINGS::PROGRAM_INFO::ORGANIZATION_NAME(QObject::tr("DB&DG"));
+QString GLOBAL_STRINGS::PROGRAM_INFO::PROGRAM_VERSION(QObject::tr("0.1"));
